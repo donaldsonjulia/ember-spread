388c66dfb961e38834d80fab73f157e9115f121b
Jeremy Brown <jeremy@notmessenger.com>
2018-04-02 11:23:11 -0500

    Initial run of ember-cli-upgrade.  App can stand up but tests do not run.  Code mods have not been ran yet

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -1,6 +1,4 @@
-import Ember from 'ember'
-const {Router: EmberRouter} = Ember
-
+import EmberRouter from '@ember/routing/router'
 import config from './config/environment'
 
 const Router = EmberRouter.extend({
@@ -8,7 +6,7 @@ const Router = EmberRouter.extend({
   rootURL: config.rootURL
 })
 
-Router.map(function () {
+Router.map(function() {
 })
 
 export default Router
